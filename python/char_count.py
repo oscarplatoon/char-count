579f9adce44f75b21b1c983fe120f413f7eee569
Shawn Kiernan <kiernan.shawn@gmail.com>
2021-05-28 15:34:51 -0500

    Shawn's python solution with unittest

--- python/char_count.py
+++ python/char_count.py
@@ -1,2 +1,27 @@
 def char_count(str):
-  # Your code here
+  
+  
+  # create a DICTIONARY to store CHAR counts
+  char_count_dictionary = {}
+  
+  
+
+  # Remove spaces from STR and convert to lower case
+  str = str.replace(" ", "").lower()
+
+  # for each CHAR in STR if CHAR is not in DICTIONARY add CHAR as key and set value to 1
+  # else add 1 to CHAR's value in dictionary
+  for char in str:
+    if char in char_count_dictionary:
+      char_count_dictionary[char] += 1
+    else:
+      char_count_dictionary[char] = 1
+  
+  # sort the dictionary by value
+  char_count_dictionary = dict(sorted(char_count_dictionary.items(), key= lambda item:item[1], reverse=True))
+  
+  # return the sorted dictionary 
+  return char_count_dictionary
+
+ 
+print(dict(char_count("an apple a day will keep the doctor away")))
