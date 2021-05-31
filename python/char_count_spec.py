18e80834c3f25caf62fa934d7b574c21d99c0a70
Marc Stanley <marcson909@gmail.com>
2021-05-31 14:27:41 -0500

    added test to check that output is dict

--- python/char_count_spec.py
+++ python/char_count_spec.py
@@ -5,6 +5,9 @@ from char_count import char_count
 
 class CharCountTestCase(unittest.TestCase):
 
+  def test_dict_output(self):
+    self.assertIsInstance(char_count("aaa"), dict)
+  
   def test_aaabbc(self):
     self.assertEqual(char_count("aaabbc"), {
   "a": 3,
