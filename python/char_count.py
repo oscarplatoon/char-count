05d85d4c49c2a51d686986bf6a3b8bfedc1bbec5
ver2point0 <jmci10@hotmail.com>
2021-06-02 23:26:12 -0700

    John char-count python

--- python/char_count.py
+++ python/char_count.py
@@ -1,2 +1,17 @@
 def char_count(str):
-  # Your code here
+	char_dict = {}
+
+	str = str.replace(' ', '')
+
+	for i in str:
+		if i in char_dict:
+			char_dict[i] += 1
+		else:
+			char_dict[i] = 1
+
+	sorted_dict = dict(sorted(char_dict.items(), key=lambda x: x[1], reverse=True))
+
+	return sorted_dict
+
+print(char_count("aaabbc"))
+print(char_count("an apple a day will keep the doctor away"))
